f5975a38a163c0822f55f9d79ebbe7ca7dbf1c7f
Luke Guan <lukeguan@outlook.com>
2022-08-20 19:51:24 -0700

    schema and query

---
+++ server.js
@@ -0,0 +1,22 @@
+import { ApolloServer, gql } from 'apollo-server'
+
+const typeDefs = `
+    schema {
+        query: Query
+    }
+    
+    type Query {
+        greeting: String
+    }
+`;
+
+
+const resolvers = {
+    Query: {
+        greeting: () => 'Hello world!',
+    },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+const { url } = await server.listen({ port: 9000 });
+console.log(`Server Running at ${url}`)
